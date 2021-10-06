0b5d717dedaaab267f6c21993df9485e1b4108ac
Gustavo Marin <gustavo.marin@intelygenz.com>
2021-10-06 09:40:14 +0200

    update hello component

--- hello-world/main.py
+++ hello-world/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World 114'
+    return 'Hello World 115'
 
 
 @app.errorhandler(500)
