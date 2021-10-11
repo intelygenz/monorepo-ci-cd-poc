998a08c4e87219114256c007e5138c1daea8bb73
Alejandro Gonzalez <alejandro.gonzalez@intelygenz.com>
2021-10-11 14:05:40 +0200

    feat(byebye): output 13

--- byebye-world/main.py
+++ byebye-world/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Bye bye world 12'
+    return 'Bye bye world 13'
 
 
 @app.errorhandler(500)
