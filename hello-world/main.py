d846472df870083075f91a32aedc4c19c4dae59c
Borja Lopez <borja.lopez@intelygenz.com>
2021-10-13 13:23:27 +0200

    fix(hello): Fix output

--- hello-world/main.py
+++ hello-world/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World 135.'
+    return 'Hello World 135'
 
 
 @app.errorhandler(500)
