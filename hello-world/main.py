aab05e1092d81d478b1b3470e00edad5673e24b7
Alejandro Gonzalez <alejandro.gonzalez@intelygenz.com>
2021-10-18 09:02:41 +0200

    feat(hello): output 14

--- hello-world/main.py
+++ hello-world/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World 145'
+    return 'Hello World 146'
 
 
 @app.errorhandler(500)
