50500ebb2b41f65eff1b7115d0418c5cce3c5e30
author <author@email.com>
2021-11-11 12:40:36 +0100

    test commit message

--- .github/actions/src/version-file-updater.test.js
+++ .github/actions/src/version-file-updater.test.js
@@ -1,9 +1,11 @@
 const updaterMod = require('./version-file-updater');
 const fs = require('fs');
+const exec = require('@actions/exec');
 
 describe('update file with version', () => {
 
   fs.writeFile = jest.fn();
+  exec.exec = jest.fn();
 
   const version = 'v3.4';
   const branch = 'main';
@@ -11,12 +13,13 @@ describe('update file with version', () => {
   const author = "author";
   const authorEmail = "author@email.com";
 
-  test('find last', async () => {
+  test('two writes for an array of two files', async () => {
     const updater = updaterMod();
     const files = '[{"file": "metaapp/values.yaml", "property": "app.tag"}, {"file": "metaapp/Chart.yaml", "property": "appVersion"}]'
     updater.updateVersionInFileAndCommit(files, version, branch, commitMessage, author, authorEmail)
 
-    expect(fs.writeFile).toHaveBeenCalledTimes(1);
+    expect(fs.writeFile).toHaveBeenCalledTimes(2);
+    expect(exec.exec).toHaveBeenCalledTimes(2);
   });
 
 });
