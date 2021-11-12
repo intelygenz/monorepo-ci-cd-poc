2094ebd7f9cb230421f1dcd947ac57c4e27d5f6d
author <author@email.com>
2021-11-12 09:09:05 +0100

    feat(hello): new output

--- hello-world/main.py
+++ hello-world/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World 162'
+    return 'Hello World 163'
 
 
 @app.errorhandler(500)
